991f2242ca4065fce53fa1b0d5a5e9df80de4718
Bobbette <Boobs@Macintosh.local>
2013-04-21 17:49:55 -0400

    Initial Commit

---
+++ SpicesCollectionView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SpicesCollectionView
+//
+//  Created by Bobbette on 4/21/13.
+//  Copyright (c) 2013 Bobbette Vance-Cochran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SpicesCollectionView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpicesCollectionView
+//
+//  Created by Bobbette on 4/21/13.
+//  Copyright (c) 2013 Bobbette Vance-Cochran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
